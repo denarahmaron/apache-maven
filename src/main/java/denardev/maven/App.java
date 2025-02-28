ac9101eadafcfed7e000090c741aa3715294fb6c
Denar_Ahmaron <denarahmaron1@gmail.com>
2025-02-28 16:04:34 +0700

    add dependency

--- src/main/java/denardev/maven/App.java
+++ src/main/java/denardev/maven/App.java
@@ -1,10 +1,17 @@
 package denardev.maven;
 
+import com.google.gson.Gson;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Gson gson = new Gson();
+
+        Person person = new Person("Denar Ahmaron");
+
+        String json = gson.toJson(person);
+        System.out.println(json);
     }
 }
